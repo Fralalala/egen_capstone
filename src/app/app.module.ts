eab5749b4eb8568faf9fae1eb2a3282c2c9d2ca9
Rolando Morales <rolando.moralesjr21@gmail.com>
2022-05-31 02:49:22 -1000

    Sign up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './routers/app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SignupComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
